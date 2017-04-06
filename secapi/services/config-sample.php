9c0df88489ac63905b3c3bb09fc964f7715b6260
enridaga <enricodaga@gmail.com>
2017-04-06 09:07:13 +0100

    added config sample for ecapi

---
+++ secapi/services/config-sample.php
@@ -0,0 +1,15 @@
+<?php
+
+$baseNS = "https://data.mksmart.org/setmp/";
+$ecapiKey = "ecapi key here";
+$ecapiSKey = "ecapi s key here";
+$ecapiODKey = "ecapi od here";
+
+$sparqlendpoint = "sparql endpoint here";
+
+$chunkSize = 100;
+
+$couchdburl = "";
+$couchdb  = '';
+$couchdbuser = '';
+$couchdbpw = '';
