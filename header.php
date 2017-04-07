526e01a258b20e073e7c4026fffa63309e8600d4
enridaga <enricodaga@gmail.com>
2017-04-07 18:32:41 +0100

    no forced https in links

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>    
         <meta property="og:title" content="MK Insight" />
         <meta property="og:site_name" content="MK Insight"/>
-        <meta property="og:url" content="https://insight.mksmart.org/" />
+        <meta property="og:url" content="http://mkinsight.org/" />
         <meta property="og:description" content="">
         <meta property="og:type" content="article" />
         <meta property="og:image" content="" />
@@ -27,21 +27,21 @@
         <![endif]-->
 		<?php wp_head(); ?>
 	<!-- MDA -->
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
    <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/mkio2/canvas/canvasjs.min.js"></script>
    <!-- //code.jquery.com/jquery-1.11.3.min.js -->
-   <script src="https://cdn.datatables.net/1.10.9/js/jquery.dataTables.min.js"></script>
-   <script src="https://cdn.datatables.net/buttons/1.0.3/js/dataTables.buttons.min.js"></script>
-   <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/2.5.0/jszip.min.js"> </script>
+   <script src="//cdn.datatables.net/1.10.9/js/jquery.dataTables.min.js"></script>
+   <script src="//cdn.datatables.net/buttons/1.0.3/js/dataTables.buttons.min.js"></script>
+   <script src="//cdnjs.cloudflare.com/ajax/libs/jszip/2.5.0/jszip.min.js"> </script>
    <!-- 
    //cdn.rawgit.com/bpampuch/pdfmake/0.1.18/build/pdfmake.min.js
    //cdn.rawgit.com/bpampuch/pdfmake/0.1.18/build/vfs_fonts.js -->
-   <script src="https://cdn.datatables.net/buttons/1.0.3/js/buttons.html5.min.js"></script>
+   <script src="cdn.datatables.net/buttons/1.0.3/js/buttons.html5.min.js"></script>
    <script src="<?php echo get_template_directory_uri(); ?>/mkio2/js/ecapi.js"></script>
    <script src="<?php echo get_template_directory_uri(); ?>/mkio2/js/mkio_config.js"></script>
    <script src="<?php echo get_template_directory_uri(); ?>/mkio2/js/mkio.js"></script>
    <script src="<?php echo get_template_directory_uri(); ?>/mkio2/js/mkinsight.js"></script>
-   <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.2/jspdf.debug.js"></script>
+   <script src="//cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.2/jspdf.debug.js"></script>
    <link href="<?php echo get_template_directory_uri(); ?>/mkio2/mkio2.css" rel="stylesheet">
    <script src="http://cdn.leafletjs.com/leaflet-0.7.5/leaflet.js"></script>
    
@@ -53,7 +53,7 @@
             <div class="container">
                 <div class="row">
                     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                        <div class="mki-icon"><a class="page-scroll navbar-brand" href="http://mkinsight.org" title="MK Insight"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/MKInsight-logo.png" height="60" alt="MK Insight - Logo"></a></div>
+                        <div class="mki-icon"><a class="page-scroll navbar-brand" href="<?php print home_url(); ?>" title="MK Insight"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/MKInsight-logo.png" height="60" alt="MK Insight - Logo"></a></div>
                         <div class="navbar-header page-scroll">
                             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-mki">
                                 <span class="sr-only">Toggle navigation</span>
