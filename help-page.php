be70e24ce5a58dc0058c1530484c0d67b23fdbdc
Alessio Antonini <dottorpollo@gmail.com>
2018-03-09 09:22:16 +0000

    add acors and bullet list to the help page

--- help-page.php
+++ help-page.php
@@ -11,16 +11,25 @@
     </h1>
     <article style="margin-bottom: 80px;">
         <p>
-            This page explains the basic functions of the MK Insight website, including how to navigate it and find
-            content and data, as well as how to contribute new content, visualise the data, and create shareable “data pages”.
+            <?php _e('This page explains the basic functions of the MK Insight website, including how to navigate it and find
+            content and data, as well as how to contribute new content, visualise the data, and create shareable “data pages”.',"mki"); ?>
         </p>
-        <p>
-            If you are experiencing problems and cannot find answer on this page, please use the <a href="#contact">contact
-                form</a> at the bottom of the page. Please describe your problem precisely.
-        </p>
+        <ul>
+            <li>
+                <?php _e("<a href='#quick-facts'>Quick facts</a> about MK Insight.","mki"); ?>
+            </li>
+            <li>
+                <?php _e("<a href='#faq'>Frequent Asked Questions</a> section.","mki"); ?>
+            </li>
+            <li>
+                <?php _e('If you are experiencing problems and cannot find answer on this page, please use the <a href="#contact">contact
+                    form</a> at the bottom of the page. Please describe your problem precisely.',"mki"); ?>
+            </li>
+        </ul>
     </article>
     <?php get_template_part('quickfacts-page'); ?>
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    <div id="faq">
         <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
             <h2 id="qea"><?php _e("Frequently Asked Questions", "mki"); ?></h2>
             <section class="entry-content">
@@ -31,14 +40,14 @@
                 <div class="entry-links"><?php wp_link_pages(); ?></div>
             </section>
         </article>
-
+    </div>
         <?php if (!post_password_required()) comments_template('', true); ?>
     <?php endwhile; endif; ?>
 
     <h2 id="contact"><?php _e("Contact","mki");?></h2>
     <p>
-        If you didn't find an answer to your question or a solution to your problem above, please use to contact form
-        below to describe what you are trying to do and why it does not work.
+        <?php _e('If you didn\'t find an answer to your question or a solution to your problem above, please use to contact form
+        below to describe what you are trying to do and why it does not work.', "mki"); ?>
     </p>
     <p><?php echo do_shortcode("[contact]"); ?></p>
 </section>
